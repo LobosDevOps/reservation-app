fea0091073a879f1037ce1a74bb212b383965cb5
ryoichi <tachibana.ryoichi@lobos-works.com>
2023-04-20 17:24:47 +0900

    product-list and detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'detail', component: ProductDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
