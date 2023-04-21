0d5fb92e340a252e9438403a85dccd9ef8c1f6ed
ryoichi <tachibana.ryoichi@lobos-works.com>
2023-04-21 10:23:52 +0900

    add product module

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProductRoutingModule } from './product-routing.module';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product.component';
+
+const routes: Routes = [
+  { path: 'products', component: ProductComponent,
+    children: [
+      { path: '', component: ProductListComponent },
+      { path: 'detail', component: ProductDetailComponent },
+    ]
+  },
+];
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    ProductDetailComponent,
+    ProductListComponent,
+  ],
+  imports: [
+    CommonModule,
+    ProductRoutingModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class ProductModule { }
