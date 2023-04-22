71039331418561516aa194e678f5b356b2ae087e
ryoichi <tachibana.ryoichi@lobos-works.com>
2023-04-22 20:40:51 +0900

    product detail

---
+++ src/app/data/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    discription: 'A large phone with one of the best screens.'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    discription: 'A great phone with one of the best camres.'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    discription: 'A standard phone.'
+  },
+  {
+    name: 'Phone Special',
+    price: 99,
+    discription: 'A special phone.'
+  },
+];
--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
+import { products } from './../../data/products';
+import { first } from 'rxjs';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+  product: any;
 
   ngOnInit(): void {
+    this.route.paramMap.pipe(first()).subscribe((params: ParamMap) => {
+      const productId = params?.get('productId');
+      if(productId){
+        this.product = products[+productId];
+      }
+    });
   }
 
 }
