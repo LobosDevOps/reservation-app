8da19e72670c9afbb18c5400f354d9ea391d6487
ryoichi <tachibana.ryoichi@lobos-works.com>
2023-04-22 22:07:19 +0900

    list and detail

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { products } from './../../data/products';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-  products: any = [1, 2, 3, 4];
+  products: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.products = products;
   }
 
 }
